b0d8f106155c1e1e858afc421cce2c9650ef6c21
fonqri <behnamsabaghi@gmail.com>
2022-05-03 19:44:01 +0430

    benchmark

--- SimpleClientTest.cpp
+++ SimpleClientTest.cpp
@@ -10,50 +10,82 @@
 
 int main()
 {
-    std::cout << "Hello Client!" << std::endl;
+	std::cout << "Hello Client!" << std::endl;
+	trantor::InetAddress server_addr(8500, true);
+	std::atomic_uint64_t counter = 0;
 
-    auto event_loop = std::make_shared<trantor::EventLoop>();
+	for (size_t i = 0; i < 3; ++i)
+	{
 
-    if (!event_loop->isInLoopThread())
-    {
-        event_loop->moveToCurrentThread();
-    }
-    trantor::InetAddress server_addr(8500);
+		std::thread([&server_addr, &counter] {
+			auto event_loop = std::make_shared<trantor::EventLoop>();
 
-    auto tcp_client_ptr = std::make_shared<trantor::TcpClient>(event_loop.get(), server_addr,
-                                                               "httpClient");
+			if (!event_loop->isInLoopThread())
+			{
+				event_loop->moveToCurrentThread();
+			}
 
-    tcp_client_ptr->setConnectionCallback([](const trantor::TcpConnectionPtr& connPtr) {
-        if (connPtr->connected())
-        {
-            std::string data{"O_o"};
-            trantor::MsgBuffer buffer;
-            buffer.append(data.c_str(), data.size());
+			const size_t connection_count = 10000;
 
-            if (connPtr->connected())
-            {
-                connPtr->send(std::move(buffer));
-            }
-        }
-        else
-        {
-            std::cerr << "connection closed " << std::endl;
-        }
-    });
-    tcp_client_ptr->setConnectionErrorCallback(
-        []() { std::cerr << "connection error " << std::endl; });
-    tcp_client_ptr->setMessageCallback(
-        [](const trantor::TcpConnectionPtr& connPtr, trantor::MsgBuffer* msg) {
-            std::cerr << "data received  " << msg->read(4096) << std::endl;
-            connPtr->shutdown();
-        });
+			std::vector<std::shared_ptr<trantor::TcpClient>> connections(connection_count);
 
-    tcp_client_ptr->setWriteCompleteCallback([](const trantor::TcpConnectionPtr& connection_ptr) {
-        std::clog << "byte sent " << connection_ptr->bytesSent() << std::endl;
-    });
+			for (size_t i = 0; i < connection_count; ++i)
+			{
 
-    tcp_client_ptr->connect();
+				connections[i] = std::make_shared<trantor::TcpClient>(event_loop.get(), server_addr,
+																	  "httpClient");
 
-    event_loop->loop();
-    return 0;
+				const auto &tcp_client_ptr = connections[i];
+
+				tcp_client_ptr->setConnectionCallback(
+					[&event_loop, &counter](const trantor::TcpConnectionPtr &connPtr) {
+						if (connPtr->connected())
+						{
+
+							event_loop->runEvery(3, [&, connPtr]() {
+								static trantor::MsgBuffer buffer;
+								buffer.retrieveAll();
+								++counter;
+								buffer.appendInt64(counter);
+								connPtr->send(std::move(buffer));
+							});
+						}
+						else
+						{
+							std::cerr << "connection closed " << std::endl;
+						}
+					});
+				tcp_client_ptr->setConnectionErrorCallback(
+					[]() { std::cerr << "connection error " << std::endl; });
+				tcp_client_ptr->setMessageCallback(
+					[](const trantor::TcpConnectionPtr &connPtr, trantor::MsgBuffer *msg) {
+						//			++cc;
+						//			if (cc > 100)
+						//			{
+						//				connPtr->shutdown();
+						//			}
+						auto counter = msg->readInt64();
+						//			++counter;
+						std::cerr << "data received  " << counter << std::endl;
+						//			trantor::MsgBuffer buffer;
+						//			buffer.appendInt64(counter);
+
+						//			connPtr->send(std::move(buffer));
+
+						//            connPtr->shutdown();
+					});
+
+				tcp_client_ptr->setWriteCompleteCallback(
+					[](const trantor::TcpConnectionPtr &connection_ptr) {
+						std::clog << "byte sent " << connection_ptr->bytesSent() << std::endl;
+					});
+
+				tcp_client_ptr->connect();
+			}
+			event_loop->loop();
+		}).detach();
+	}
+
+	std::cin.get();
+	return 0;
 }
--- SimpleServerTest.cpp
+++ SimpleServerTest.cpp
@@ -3,59 +3,90 @@
 *  Copyright (C) 2021-3-16 ModernCpp.ir . All Rights Reserved.  *
 ***************************************************/
 
-#include <iostream>
-
 #include <trantor/net/EventLoop.h>
 #include <trantor/net/TcpServer.h>
 
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+size_t counter;
+
+size_t connection_count = 30000;
+
 int main()
 {
-    std::cout << "Hello Server!" << std::endl;
+	auto start = std::chrono::high_resolution_clock::now();
+	auto last_time = std::chrono::high_resolution_clock::now();
 
-    auto event_loop = std::make_shared<trantor::EventLoop>();
+	std::cout << "Hello Server!" << std::endl;
 
-    if (!event_loop->isInLoopThread())
-    {
-        event_loop->moveToCurrentThread();
-    }
-    trantor::InetAddress server_addr(8500);
+	auto event_loop = std::make_shared<trantor::EventLoop>();
 
-    auto server = std::make_shared<trantor::TcpServer>(event_loop.get(), server_addr, "httpClient");
+	if (!event_loop->isInLoopThread())
+	{
+		event_loop->moveToCurrentThread();
+	}
 
-    server->setConnectionCallback([](const trantor::TcpConnectionPtr& connPtr) {
-        std::clog << __LINE__ << __FUNCTION__ << std::endl;
-        if (connPtr->connected())
-        {
-            std::clog << __LINE__ << __FUNCTION__ << std::endl;
-            LOG_DEBUG << "new connection from " << connPtr->peerAddr().toIpPort();
-            auto size = connPtr->bytesReceived();
-            auto buff = connPtr->getRecvBuffer();
+	trantor::InetAddress server_addr("0.0.0.0", 8500);
 
-            std::clog << "received data : \n" << buff->read(size);
+	auto server = std::make_shared<trantor::TcpServer>(event_loop.get(), server_addr, "httpClient");
 
-            connPtr->send(*buff);
-        }
-        else if (connPtr->disconnected())
-        {
-            std::clog << __LINE__ << __FUNCTION__ << std::endl;
-            LOG_DEBUG << "conn disconnected!" << connPtr->peerAddr().toIpPort();
-        }
-    });
-    server->setRecvMessageCallback(
-        [](const trantor::TcpConnectionPtr& connPtr, trantor::MsgBuffer* buff) {
-            std::clog << __LINE__ << __FUNCTION__ << std::endl;
+	server->setIoLoopNum(8);
 
-            auto size = connPtr->bytesReceived();
+	server->setConnectionCallback([](const trantor::TcpConnectionPtr &connPtr) {
+		//        std::clog << __LINE__ << __FUNCTION__ << std::endl;
+		if (connPtr->connected())
+		{
+			//            std::clog << __LINE__ << __FUNCTION__ << std::endl;
+			//            LOG_DEBUG << "new connection from " << connPtr->peerAddr().toIpPort();
+			//			auto size = connPtr->bytesReceived();
+			//			auto buff = connPtr->getRecvBuffer();
 
-            std::string data = buff->read(size);
+			//            std::clog << "received data : \n" << buff->read(size);
 
-            std::clog << "received data : \n" << data;
+			//			connPtr->send(*buff);
+		}
+		else if (connPtr->disconnected())
+		{
+			//            std::clog << __LINE__ << __FUNCTION__ << std::endl;
+			std::clog << counter << " message count ping/pong" << std::endl;
+			LOG_DEBUG << "conn disconnected!" << connPtr->peerAddr().toIpPort();
+		}
+	});
+	server->setRecvMessageCallback(
+		[&start, &last_time](const trantor::TcpConnectionPtr &connPtr, trantor::MsgBuffer *buff) {
+			//            std::clog << __LINE__ << __FUNCTION__ << std::endl;
 
-            connPtr->send(std::move(data));
-        });
+			counter = buff->readInt64();
 
-    server->start();
+			if (counter % connection_count == 0)
+			{
 
-    event_loop->loop();
-    return 0;
+				// record end time
+				auto now = std::chrono::high_resolution_clock::now();
+
+				std::cout << "one iterate time: "
+						  << std::chrono::duration<double>(now - last_time).count() << " s"
+						  << std::endl;
+				std::cout << "overall time: " << std::chrono::duration<double>(now - start).count()
+						  << " s" << std::endl;
+
+				last_time = now;
+
+				std::clog << counter << std::endl;
+			}
+
+			trantor::MsgBuffer buffer;
+			buffer.appendInt64(counter);
+
+			//			connPtr->send(std::move(buffer));
+		});
+
+	server->start();
+
+	event_loop->loop();
+	return 0;
 }
